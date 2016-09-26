213b528b6f4ef63d8256d78fbb07679835d2a13b
abdel792 <abdelkrim.bensaid@gmail.com>
2016-09-26 20:38:56 +0200

    Adding Ctrl + F8 for Toggle mode and improvements in the listbox available with Ctrl + L

--- forPython.py
+++ forPython.py
@@ -1,9 +1,94 @@
-import re
+# coding:utf-8
 
+import re
 sp=sixpad
+
 regFunc = re.compile("^[ \t]*def.*?:.*")
 regClass = re.compile("^[ \t]*class.*?:.*")
 regClassAndFunc = re.compile("^[ \t]*((?:class|def).*?:.*$)", re.MULTILINE)
+mode = 0
+
+def toggleMode():
+	global mode
+	if mode == 0:
+		mode = 1
+		sp.say("Dire les numéros de lignes", True)
+	elif mode == 1:
+		mode = 2
+		sp.say("Dire les niveaux", True)
+	elif mode == 2:
+		mode = 3
+		sp.say("Dire les numéros de lignes et les niveaux", True)
+	elif mode == 3:
+		mode = 0
+		sp.say("Ne rien dire", True)
+sp.window.addAccelerator("CTRL+F8", toggleMode)
+
+def onKeyDown(activePage, vk):
+	if vk == 38 and not activePage.selectedText:
+		lineNumber = activePage.curLine - 1
+		if lineNumber < 0:
+			lineNumber = 0
+		activePage.curLine = lineNumber 
+		sp.say(getLineHeading(activePage.curLine), True)
+		return False
+	if vk == 40 and not activePage.selectedText:
+		lineNumber = activePage.curLine + 1
+		if lineNumber == activePage.lineCount:
+			lineNumber = activePage.lineCount - 1
+		activePage.curLine = lineNumber
+		sp.say(getLineHeading(activePage.curLine), True)
+		return False
+	if vk == 547 and not activePage.selectedText:
+		activePage.curLine = activePage.lineCount - 1
+		sp.say(getLineHeading(activePage.curLine), True)
+		return False
+	if vk == 548 and not activePage.selectedText:
+		activePage.curLine = 0
+		sp.say(getLineHeading(activePage.curLine), True)
+		return False
+	if vk == 33 and not activePage.selectedText:
+		lineNumber = activePage.curLine - 16
+		if lineNumber < 0:
+			lineNumber = 0
+		activePage.curLine = lineNumber
+		sp.say(getLineHeading(activePage.curLine), True)
+		return False
+	if vk == 34 and not activePage.selectedText:
+		lineNumber = activePage.curLine + 16
+		if lineNumber >= activePage.lineCount:
+			lineNumber = activePage.lineCount - 1
+		activePage.curLine = lineNumber 
+		sp.say(getLineHeading(activePage.curLine), True)
+		return False
+	return True
+
+def getLineHeading(line):
+	lineNumber = str(line+1) + ". "
+	level = "niveau " + str(sp.window.curPage.lineIndentLevel(line)) + ". "
+	lineAndLevel = lineNumber + level
+	if mode == 0:
+		sayLine = ""
+		sayLevel = ""
+		sayLineAndLevel = ""
+	elif mode == 1:
+		sayLine = lineNumber
+		sayLevel = ""
+		sayLineAndLevel = ""
+	elif mode == 2:
+		sayLine = ""
+		sayLevel = level
+		sayLineAndLevel = ""
+	elif mode == 3:
+		sayLine = ""
+		sayLevel = ""
+		sayLineAndLevel = lineAndLevel
+	return "%s%s%s%s" % (sayLine, sayLevel, sayLineAndLevel, sp.window.curPage.line(line))
+sp.window.curPage.addEvent("keyDown", onKeyDown)
+
+def openedPage(newPage):
+	newPage.addEvent("keyDown", onKeyDown)
+sp.window.addEvent("pageOpened", openedPage)
 
 def parseElement(element):
 	offset=sp.window.curPage.text.index(element)
@@ -25,8 +110,8 @@ def nextElement():
 			sp.window.messageBeep(0)
 			break
 	sp.window.curPage.curLine = i
-	sp.say(sp.window.curPage.line(sp.window.curPage.curLine), True)
-sixpad.window.addAccelerator("F2", nextElement)
+	sp.say(getLineHeading(sp.window.curPage.curLine), True)
+sp.window.addAccelerator("F2", nextElement)
 
 def previousElement():
 	if regClassAndFunc.match(sp.window.curPage.line(sp.window.curPage.curLine)) and sp.window.curPage.curLine > 0:
@@ -39,20 +124,28 @@ def previousElement():
 			sp.window.messageBeep(0)
 			break
 	sp.window.curPage.curLine = i
-	sp.say(sp.window.curPage.line(sp.window.curPage.curLine), True)
-sixpad.window.addAccelerator("SHIFT+F2", previousElement)
+	sp.say(getLineHeading(sp.window.curPage.curLine), True)
+sp.window.addAccelerator("SHIFT+F2", previousElement)
 
 def selectAClassOrFunction():
-	choices=regClassAndFunc.findall(sixpad.window.curPage.text)
+	choices=regClassAndFunc.findall(sp.window.curPage.text)
 	if choices:
+		choices=[k.strip() for k in choices]
 		choicesList=[parseElement(k) for k in choices]
-		element=sixpad.window.choice("Veuillez sélectionner une classe ou fonction", "Liste d'éléments", choicesList, 0)
+		i = sp.window.curPage.curLine
+		while i > -1 and not regClassAndFunc.match(sp.window.curPage.line(i)):
+			i -= 1
+			if i == -1:
+				i = sp.window.curPage.text.index(choices[0])
+				i = sp.window.curPage.lineOfOffset(i)
+				break
+		element=sp.window.choice("Veuillez sélectionner une classe ou fonction", "Liste d'éléments", choicesList, choices.index(sp.window.curPage.line(i).strip()))
 		if element == -1:
 			return
-		offset=sixpad.window.curPage.text.index(choices[element])
-		lineNumber=sixpad.window.curPage.lineOfOffset(offset)
-		sixpad.window.curPage.curLine = lineNumber
-		sixpad.say(sixpad.window.curPage.line(lineNumber), True)
+		offset=sp.window.curPage.text.index(choices[element])
+		lineNumber=sp.window.curPage.lineOfOffset(offset)
+		sp.window.curPage.curLine = lineNumber
+		sp.say(getLineHeading(lineNumber), True)
 	else:
-		sixpad.say("Aucune classe ou fonction trouvée !", True)
-sixpad.window.addAccelerator("CTRL+L", selectAClassOrFunction)
+		sp.say("Aucune classe ou fonction trouvée !", True)
+sp.window.addAccelerator("CTRL+L", selectAClassOrFunction)
